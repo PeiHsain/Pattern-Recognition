ea20722cb94bf6ae419d1f3a4076e4faeda19405
TsaiPeiHsain <susan4323@gmail.com>
2022-04-05 00:53:38 +0800

    all done

--- HW2/A102548_HW2.py
+++ HW2/A102548_HW2.py
@@ -11,8 +11,6 @@
 # 
 # Please note that only **NUMPY** can be used to implement your model, you will get no points by simply calling sklearn.discriminant_analysis.LinearDiscriminantAnalysis 
 
-from audioop import mul
-from operator import indexOf
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -25,9 +23,10 @@ MAX = 1000000000
 def Mean(data):
     'Calculate the mean of data set.\nOutput : value of the mean'
     m = [0, 0]
-    for i in range(len(data)):
-        m += data[i]
-    m /= len(data)
+    for i in data:
+        m += i
+    if len(data) != 0:
+        m /= len(data)
     return m
 
 
@@ -40,8 +39,7 @@ def KNN(test, trainX, trainY, K):
         minY = [0]
         for i in range(len(trainX)):
             dis = abs(x-trainX[i])
-            # print(trainX[i])
-            dis = (dis[0][0]**2) + (dis[0][1]**2)
+            dis = (dis[0]**2) + (dis[1]**2)
             if dis < max(minX):
                 if len(minX) >= K:
                     # remove max value of min
@@ -71,7 +69,6 @@ if __name__ == "__main__":
     x_test = pd.read_csv("x_test.csv").values
     y_test = pd.read_csv("y_test.csv").values[:,0]
 
-
     # ## 1. Compute the mean vectors mi, (i=1,2) of each 2 classes
     # seperate the class of training data in N1, N2
     N1 = []
@@ -90,19 +87,19 @@ if __name__ == "__main__":
     print(f"mean vector of class 1: {m1}", f"\nmean vector of class 2: {m2}")
 
 
-    # ## 2. Compute the Within-class scatter matrix SW = sum((x-m1) * (x-m1)T) + sum((x-m2) * (x-m2)T)
-    sw1 = 0
-    sw2 = 0
+    # ## 2. Compute the Within-class scatter matrix SW = sum((x1-m1) * (x1-m1)T) + sum((x2-m2) * (x2-m2)T)
+    sw1 = np.zeros([2, 2])
+    sw2 = np.zeros([2, 2])
     # for class1
     for x in N1:
-        mat = x-m1
+        mat = x - m1
         mat = mat.reshape((len(mat), 1))
-        sw1 += np.dot(mat, mat.T)
+        sw1 += np.matmul(mat, mat.T)
     # for class2
     for x in N2:
-        mat = x-m2
+        mat = x - m2
         mat = mat.reshape((len(mat), 1))
-        sw2 += np.dot(mat, mat.T)
+        sw2 += np.matmul(mat, mat.T)
     sw = sw1 + sw2
     assert sw.shape == (2,2)
     print(f"Within-class scatter matrix SW: {sw}")
@@ -111,29 +108,31 @@ if __name__ == "__main__":
     # ## 3.  Compute the Between-class scatter matrix SB = (m2-m1) * (m2-m1)T
     matrix = m2 - m1
     matrix = matrix.reshape((len(matrix), 1))
-    sb = np.dot(matrix, matrix.T)
+    sb = np.matmul(matrix, matrix.T)
     assert sb.shape == (2,2)
     print(f"Between-class scatter matrix SB: {sb}")
 
 
     # ## 4. Compute the Fisher’s linear discriminant, J(w) = (wT*SB*W) / (wT*SW*W) -> w = SW^-1 * (m2-m1)
-    w = np.dot(np.linalg.inv(sw), matrix)
+    w = np.matmul(np.linalg.inv(sw), matrix)
     assert w.shape == (2,1)
-    print(f" Fisher’s linear discriminant: {w}")
+    print(f"Fisher’s linear discriminant: {w}")
 
 
     # ## 5. Project the test data by linear discriminant to get the class prediction by nearest-neighbor rule and calculate the accuracy score 
     # you can use accuracy_score function from sklearn.metrics.accuracy_score
     # Project by linear discriminant, new x = wT * x
-    proj_x_test = []
-    for x in x_test:
-        proj_x_test.append(w.T * x.reshape((1, 2)))
-        # print(proj_x_test)
-    proj_x_train = []
-    for x in x_train:
-        proj_x_train.append(w.T * x.reshape((1, 2)))
+    # test case
+    proj_x_test = np.zeros([len(x_test), 2])
+    for i in range(len(x_test)):
+        proj_x_test[i] = w.T * x_test[i]
+    # train case
+    proj_x_train = np.zeros([len(x_train), 2])
+    for i in range(len(x_train)):
+        proj_x_train[i] = w.T * x_train[i]
+
     # predicte by nearest-neighbor rule
-    y_pred = KNN(proj_x_test, proj_x_train, y_train, 5)
+    y_pred = KNN(proj_x_test, proj_x_train, y_train, 6)
     # calculate the accuracy score
     acc = accuracy_score(y_test, y_pred)
     print(f"Accuracy of test-set {acc}")
@@ -150,29 +149,34 @@ if __name__ == "__main__":
     proj1_y = []
     proj2_x = []
     proj2_y = []
+    slope = float(w[1] / w[0])  # slope = y/x
+    intercept = 6
+
+    # Projection line
+    x_line = np.linspace(1, 3, 1000)
+    y_line = []
+    for i in x_line:
+        y_line += [slope * i + intercept]
+
+    # Classification ans project point, projection = w * (x-x0, y-y0).w / |w|^2
     for i in range(x_test.shape[0]):
+        proj = w[0] * (x_test[i][0] - x_line[0]) + w[1] * (x_test[i][1] - y_line[0])
+        proj /= (w[0]**2 + w[1]**2)
+        proj = proj * w
         # class1
         if y_pred[i] == 0:
             class1_x.append(x_test[i][0])
             class1_y.append(x_test[i][1])
             # project testing data
-            proj1_x.append(proj_x_test[i][0][0])
-            proj1_y.append(proj_x_test[i][0][1])
+            proj1_x.append(proj[0] + x_line[0])
+            proj1_y.append(proj[1] + y_line[0])
         # class2    
         elif y_pred[i] == 1:
             class2_x.append(x_test[i][0])
             class2_y.append(x_test[i][1])
             # project testing data
-            proj2_x.append(proj_x_test[i][0][0])
-            proj2_y.append(proj_x_test[i][0][1])
-
-    # Projection line
-    slope = float(w[0] / w[1])
-    intercept = 4
-    x_line = np.linspace(0, 6, 10000)
-    y_line = []
-    for i in x_line:
-        y_line += [slope * i + intercept]
+            proj2_x.append(proj[0] + x_line[0])
+            proj2_y.append(proj[1] + y_line[0])
 
     # Visualization
     plt.title(f"Projection line: w={slope}, b={intercept}")
@@ -181,5 +185,6 @@ if __name__ == "__main__":
     plt.scatter(class2_x, class2_y, color='red')
     plt.scatter(proj1_x, proj1_y, color='green')
     plt.scatter(proj2_x, proj2_y, color='yellow')
-    # plt.plot(class1, proj1, lw=1)
+    plt.plot([class1_x, proj1_x], [class1_y, proj1_y], color='gray', lw=0.3)
+    plt.plot([class2_x, proj2_x], [class2_y, proj2_y], color='gray', lw=0.3)
     plt.show()
