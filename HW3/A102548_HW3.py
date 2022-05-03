178e7754b8ac676c2cfd3a241aee604a2ba04107
TsaiPeiHsain <susan4323@gmail.com>
2022-05-04 02:21:06 +0800

    decision tree done

--- HW3/A102548_HW3.py
+++ HW3/A102548_HW3.py
@@ -19,7 +19,6 @@
 # In[2]:
 
 
-from re import L
 import pandas as pd
 import numpy as np
 file_url = "http://storage.googleapis.com/download.tensorflow.org/data/heart.csv"
@@ -138,29 +137,30 @@ test_df.head()
 def gini(sequence):
     'Compute the Gini-index for two class. The smaller, the purer.\nOutput : Gini-index'
     seq_len = len(sequence)
-    p1 = np.sum(sequence == 0) / seq_len
-    p2 = np.sum(sequence == 1) / seq_len
-
-    # Gini = 1 - sum_all_class_k(Pk^2)
-    gini = 1 - (p1**2) - (p2**2)
-    return gini
+    if seq_len <= 0:
+        return 0
+    else:
+        p1 = np.sum(sequence == 0) / seq_len
+        p2 = np.sum(sequence == 1) / seq_len
+        # Gini = 1 - sum_all_class_k(Pk^2)
+        return 1 - (float(p1)**2) - (float(p2)**2)
 
 
 def entropy(sequence):
     'Compute the entropy for two class. The smaller, the purer.\nOutput : entropy'
     seq_len = len(sequence)
-    p1 = np.sum(sequence == 0) / seq_len
-    print(p1)
-    p2 = np.sum(sequence == 1) / seq_len
-    print(p2)
+    if seq_len <= 0:
+        return 0
+    else:
+        p1 = np.sum(sequence == 0) / seq_len
+        p2 = np.sum(sequence == 1) / seq_len
+        p1 = float(p1)
+        p2 = float(p2)
 
-    if p1 == 0 or p2 == 0:  # all class are the same in one node
-        entropy = 0
-    elif p1 == p2:  # class are half-and-half
-        entropy = 1
-    else:   # Entropy = -sum_all_class_k(Pk*log2(Pk))
-        entropy = -(p1*np.log2(p1)) - (p2*np.log2(p2))    
-    return entropy
+        if (p1 == 0) | (p2 == 0):  # all class are the same in one node
+            return 0
+        else:   # Entropy = -sum_all_class_k(Pk*log2(Pk))
+            return -(p1*np.log2(p1)) - (p2*np.log2(p2))    
 
 
 # In[14]:
@@ -197,7 +197,7 @@ print("Entropy of data is ", entropy(data))
 
 
 class TreeNode():
-    def __init__(self, dataSet=None, depth=0) -> None:
+    def __init__(self, dataSet=None, depth=0):
         self.DataSet = dataSet    # input data
         self.Attribute = None # selected attribute
         self.Threshold = 0  # threshold for node
@@ -206,6 +206,20 @@ class TreeNode():
         self.Depth = depth  # depth of node
         self.Leaf = False   # is leaf node or not
         self.Gain = 0   # information gain
+
+    def Update_A_G_T(self, A, G, T):
+        self.Attribute = A
+        self.Gain = G
+        self.Threshold = T
+
+    def Set_Right(self, R):
+        self.Right = R
+
+    def Set_Left(self, L):
+        self.Left = L
+
+    def Set_Leaf(self, Le):
+        self.Leaf = Le
 
 
 class DecisionTree():
@@ -226,24 +240,39 @@ class DecisionTree():
     def SplitAttribute(self, node):
         'Select the best attribute to split data.\nOutput : best attribute index'
         max_gain = 0
-        bestfeature = 0
+        bestfeature = None
+        best_threshold = 0
         # loop all values of all features
-        for i in range(len(node.DataSet.column)-1): # remove the 'target' colcum
+        for i in range(int(node.DataSet.shape[1])-1): # remove the 'target' colcum
             # partition the node and calculate the purity of data
             # pick the col of feature[i], sort by ascending order
-            tmp_data = node.DataSet.sort_values([node.DataSet.colcum[i]], ascending=True).to_numpy
-            # split by threshold of average(i-th, (i+1)-th)
+            tmp_data = node.DataSet.sort_values(by=[node.DataSet.columns[i]], ascending=True)
             N = len(tmp_data)
-            for i in range(N-1):
-                tmp_left_split = self.SplitPurity(tmp_data[:i+1])
-                tmp_right_split = self.SplitPurity(tmp_data[i+1:])
-                after_gain = ((i+1) * tmp_left_split + (N-i-1) * tmp_right_split) / N
+            # split by threshold of average(i-th, (i+1)-th)
+            # Continuous
+            if node.DataSet.columns[i] == 'age' or node.DataSet.columns[i] == 'trestbps' or node.DataSet.columns[i] == 'chol' or node.DataSet.columns[i] == 'thalach' or node.DataSet.columns[i] == 'oldpeak':
+                for k in range(N-1):
+                    tmp_left_split = self.SplitPurity(tmp_data.loc[tmp_data.index[0:k+1], ['target']])
+                    tmp_right_split = self.SplitPurity(tmp_data.loc[tmp_data.index[k+1:N], ['target']])
+                    after_gain = ((k+1) * float(tmp_left_split) + (N-k-1) * float(tmp_right_split)) / N
+                    information_gain = node.Gain - after_gain
+                    # find the value of feature can yield lowest value of gini or entropy
+                    if information_gain > max_gain:
+                        max_gain = information_gain
+                        bestfeature = node.DataSet.columns[i]
+                        best_threshold = (tmp_data.at[tmp_data.index[k], bestfeature] + tmp_data.at[tmp_data.index[k+1], bestfeature]) / 2
+            else:
+                # Discrete
+                k = len(tmp_data[tmp_data[node.DataSet.columns[i]] < 1])
+                tmp_left_split = self.SplitPurity(tmp_data.loc[tmp_data.index[0:k], ['target']]) # data = 0
+                tmp_right_split = self.SplitPurity(tmp_data.loc[tmp_data.index[k:N], ['target']]) # data = 1
+                after_gain = (k * float(tmp_left_split) + (N-k) * float(tmp_right_split)) / N
                 information_gain = node.Gain - after_gain
                 # find the value of feature can yield lowest value of gini or entropy
                 if information_gain > max_gain:
                     max_gain = information_gain
-                    bestfeature = node.DataSet.column[i]
-                    best_threshold = (tmp_data[i] + tmp_data[i+1]) / 2
+                    bestfeature = node.DataSet.columns[i]
+                    best_threshold = 0.5
         return bestfeature, max_gain, best_threshold
 
     def SplitNode(self, node):
@@ -251,47 +280,103 @@ class DecisionTree():
         R_index = []
         L_index = []
         # Split data into left and right node by threshold
-        for i in range(len(node.DataSet[node.Attribute])):
-            if node.DataSet[node.Attribute] <= node.Threshold:
-                R_index.append(node.index[i])
+        for i in range(node.DataSet.shape[0]):
+            if node.DataSet.at[node.DataSet.index[i], node.Attribute] <= node.Threshold:
+                L_index.append(node.DataSet.index[i])
             else:
-                L_index.append(node.index[i])
-        R = pd.DataFrame(node.DataSet.iloc[R_index])
-        L = pd.DataFrame(node.DataSet.iloc[L_index])
-        # Remove the feature that had pick
-        R = R.drop([node.Attribute], axis=1)
-        L = L.drop([node.Attribute], axis=1)
-        node_R = TreeNode(R, node.Depth+1)
+                R_index.append(node.DataSet.index[i])
+        L = pd.DataFrame(node.DataSet.loc[L_index])
+        R = pd.DataFrame(node.DataSet.loc[R_index])
         node_L = TreeNode(L, node.Depth+1)
+        node_R = TreeNode(R, node.Depth+1)
+        # Check is node empty or not
+        if len(L_index) == 0:
+            node_L.Set_Leaf(True)
+        if len(R_index) == 0:
+            node_R.Set_Leaf(True)
         return node_R, node_L
 
     def GenerateTree(self, node):
         'Generate the decision tree by recursive method.'
+        # Initial gain
         node.Gain = self.SplitPurity(node.DataSet['target'])
+        # print(node.Gain)
+        # print("len ", node.DataSet.shape[0])
         # Stopping criteria
         # The data in each leaf-node belongs to the same class
         # Depth of the tree is equal to some pre-specified limit
-        if node.Gain == 0 or node.Depth >= self.Max_depth:
-            node.Leaf = True
+        if node.Gain == 0 or node.Depth >= self.Max_depth or node.Leaf == True:
+            node.Set_Leaf(True)
             return node
 
         # Until stopped
         # a. Select a node
         # b. loop all values of all features
-        node.Attribute, node.Gain, node.Threshold = self.SplitAttribute(node)    
+        A, G, T = self.SplitAttribute(node)
+        node.Update_A_G_T(A, G, T)
+        # Cannot split by the feature
+        # if node.Gain == 0:
+        #     node.Set_Leaf(True)
+        #     return node 
+        # print(node.Attribute)
         # c. Split the node using the feature value found in step b.
         right_node, left_node = self.SplitNode(node)
         # d. Go to next node and repeat step a to c.
-        node.Left = self.GenerateTree(left_node)
-        node.Right = self.GenerateTree(right_node)
+        # print("L")
+        node.Set_Left(left_node)
+        self.GenerateTree(node.Left)
+        # print("R")
+        node.Set_Right(right_node)
+        self.GenerateTree(node.Right)
 
     def Create(self, Data=None):
         'Create a initial decision tree.'
         self.Tree = TreeNode(Data)
         self.GenerateTree(self.Tree)
 
-        
+    def Vote(self, node):
+        'Majority vote for the node.\nOutput : the prediction'
+        count1 = 0
+        count2 = 0
+        for i in range(node.DataSet.shape[0]):
+            if node.DataSet.at[node.DataSet.index[i], 'target'] == 0:
+                count1 += 1
+            else:
+                count2 += 1
+        # Classifier the data to major class
+        if count1 >= count2:
+            return 0
+        else:
+            return 1
 
+    def Accuracy(self, test, test_predict):
+        'Compute and show the accuracy score.'
+        accur = 0
+        for i in range(test_num):
+            if test.at[test.index[i], 'target'] == test_predict[i]:
+                accur += 1
+        accur /= test_num
+        print(f"Accuracy score = {accur}")
+
+
+    def Testing(self, test):
+        'Put the testing data into decision tree to predicte.'
+        test_pred = np.zeros((test_num))
+        # Prediction
+        for i in range(test_num):
+            tmp_node = self.Tree
+            x = test.loc[test.index[i]]
+            # go down the decision tree untill reach the leaf
+            while tmp_node.Leaf == False:
+                if x[tmp_node.Attribute] < tmp_node.Threshold:
+                    tmp_node = tmp_node.Left    # go to left node
+                else:
+                    tmp_node = tmp_node.Right   # go to right node
+            # majority vote
+            test_pred[i] = self.Vote(tmp_node)
+        # Show the accuracy
+        self.Accuracy(test, test_pred)
+        
 
 # ### Question 2.1
 # Using `criterion=gini`, showing the accuracy score of test data by `max_depth=3` and `max_depth=10`, respectively.
@@ -300,9 +385,17 @@ class DecisionTree():
 # In[8]:
 
 
+print("Q 2.1:")
 clf_depth3 = DecisionTree(criterion='gini', max_depth=3)
 clf_depth10 = DecisionTree(criterion='gini', max_depth=10)
-
+print("Max_depth = 3:")
+clf_depth3.Create(train_encoding)
+clf_depth3.Testing(test_encoding)
+print("--------------------------------------")
+print("Max_depth = 10:")
+clf_depth10.Create(train_encoding)
+clf_depth10.Testing(test_encoding)
+print("--------------------------------------")
 
 # ### Question 2.2
 # Using `max_depth=3`, showing the accuracy score of test data by `criterion=gini` and `criterion=entropy`, respectively.
@@ -311,8 +404,17 @@ clf_depth10 = DecisionTree(criterion='gini', max_depth=10)
 # In[9]:
 
 
+print("Q 2.2:")
 clf_gini = DecisionTree(criterion='gini', max_depth=3)
 clf_entropy = DecisionTree(criterion='entropy', max_depth=3)
+print("Criterion = gini:")
+clf_gini.Create(train_encoding)
+clf_gini.Testing(test_encoding)
+print("--------------------------------------")
+print("Criterion = entropy:")
+clf_entropy.Create(train_encoding)
+clf_entropy.Testing(test_encoding)
+print("--------------------------------------")
 
 
 # - Note: Your decisition tree scores should over **0.7**. It may suffer from overfitting, if so, you can tune the hyperparameter such as `max_depth`
